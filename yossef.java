c62f42b22eacaa8481a29752f6ea54d67a40bb3e
jossefo <yossidv@hotmail.com>
2022-11-15 11:26:34 +0200

    changed 1

--- yossef.java
+++ yossef.java
@@ -3,7 +3,7 @@ public class yossef {
 	public static void main(String[] args) {
 
 		for (int i=0;i<10;i++){
-			System.out.println("Yossef test num:"+i);
+			System.out.println("Yossef test after change num:"+i);
 		}
 	}
 }
